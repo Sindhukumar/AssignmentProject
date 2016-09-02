ed6864d2f754def813c0ffd6d0c6093fa548a965
Sindhukumar <ssundar6@gmu.edu>
2016-09-02 14:44:46 -0400

    Special Character code added

---
+++ src/SpecialCharacter.java
@@ -0,0 +1,13 @@
+
+public class SpecialCharacter {
+
+	public void specialCharacterProgram(String args[]) {
+	    String message1, message2;
+	    
+	    message1 = "\\/\\/\\/\\/\\/\\r\\t\\b";
+	    message2 = "\" ;" ;
+	    
+	    System.out.println("message1 = " + message1 +"\n"+"message2 = " + message2);
+	            
+	}
+}
