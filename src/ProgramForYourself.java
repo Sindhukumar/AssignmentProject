ecabe6c2860facd37239d24cf5adbf7e43cd2f6a
Sindhukumar <ssundar6@gmu.edu>
2016-09-02 11:51:16 -0400

    inital commit

---
+++ src/ProgramForYourself.java
@@ -0,0 +1,8 @@
+
+public class ProgramForYourself {
+	public static void main(String[] args) {
+		System.out.println("Comment:");
+		System.out.println("Name: Sindhukumar Sundaram");	
+		System.out.println("Age:26");
+	}
+}
